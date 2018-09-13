67373e895ec3e8628b5e910ef61e9dea8cedc149
AnastasiaDro <adrogunova@gmail.com>
2018-09-14 00:23:55 +0300

    Добавлен (ешё в прошлом коммите) класс hello world

--- src/ru/gitpractice/test2/HelloWorld1.java
+++ src/ru/gitpractice/test2/HelloWorld1.java
@@ -2,7 +2,7 @@ package ru.gitpractice.test2;
 
 public class HelloWorld1 {
     public static void main(String[] args) {
-        System.out.println("Hello, git!");
+        System.out.println("Hello, git!!!");
     }
 
 }
